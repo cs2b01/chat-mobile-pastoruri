8407d935fe9a5686c0e511f3eee712968d169491
pastoruri <40170714+pastoruri@users.noreply.github.com>
2019-06-30 22:44:33 -0500

    plx work v2.0

---
+++ app/src/main/java/cs2901/utec/chat_mobile/ContactsActivity.java
@@ -0,0 +1,20 @@
+package cs2901.utec.chat_mobile;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        TextView contactsActivityTitle = (TextView)findViewById(R.id.contactsActivityTitle);
+        String username = getIntent().getStringExtra("username");
+        String title = username + "'s contacts";
+        contactsActivityTitle.setText(title);
+    }
+
+}
